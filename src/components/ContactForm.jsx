56d6ca20fe3f148fbd07588c99a6d72f1a971a4c
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-11-12 15:11:23 -0300

    background form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -55,7 +55,7 @@ const ContactForm = () => {
               value={formData.name}
               onChange={handleInputChange}
               placeholder="Tu nombre"
-              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full border bg-white/20 border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
           </div>
@@ -71,7 +71,7 @@ const ContactForm = () => {
               value={formData.email}
               onChange={handleInputChange}
               placeholder="Email Address"
-              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full bg-white/20 border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
           </div>
@@ -86,7 +86,7 @@ const ContactForm = () => {
             onChange={handleInputChange}
             placeholder="Escribe un mensaje..."
             rows="5"
-            className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full bg-white/20 border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
             required
           ></textarea>
         </div>
