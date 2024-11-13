4142f30f14d8b8179639ad8f9294f609955486c1
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-11-13 12:50:36 -0300

    emailjs

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,34 +1,33 @@
 import { useTranslation } from "react-i18next";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
   const { t } = useTranslation();
-  const handleSubmit = async (e) => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
-    const formData = {
-      name: e.target.name.value,
-      email: e.target.email.value,
-      message: e.target.message.value,
-    };
 
-    const response = await fetch("/.netlify/functions/sendEmail", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-
-    if (response.ok) {
-      alert("Mensaje enviado con éxito");
-    } else {
-      alert("Error al enviar el mensaje");
-    }
+    emailjs
+      .sendForm("service_q56xg3q", "template_7xdnr2c", form.current, {
+        publicKey: "NVL36_MsNJ__5erJH",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
   };
 
   return (
     <div className="bg-white/30 rounded-lg shadow-md p-6 max-w-md mx-auto">
       <h2 className="text-2xl font-bold mb-4">Get in touch</h2>
-      <form className="text-sm" method="POST" onSubmit={handleSubmit}>
+      <form className="text-sm" ref={form} onSubmit={sendEmail}>
         <div className="grid grid-cols-2 gap-4">
           <div className="mb-4">
             <label
@@ -37,7 +36,7 @@ const ContactForm = () => {
             ></label>
             <input
               id="name"
-              name="name"
+              name="user_name"
               type="text"
               placeholder="Tu nombre"
               className="w-full border bg-white/20 border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -51,7 +50,7 @@ const ContactForm = () => {
             ></label>
             <input
               id="email"
-              name="email"
+              name="user_email"
               type="email"
               placeholder="Email Address"
               className="w-full bg-white/20 border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
