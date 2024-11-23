4367060402ed38b145569a7f4d98aebe7016c79a
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-11-23 15:08:50 -0300

    intersection observer

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,44 +1,38 @@
-import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
 import EmailCopyButton from "./EmailCopyButton";
 import { AiOutlineDownload } from "react-icons/ai";
 import SocialLinks from "./SocialLinks";
+import AnimatedComponent from "./AnimatedComponent";
 
 const Links = () => {
   const { t } = useTranslation(); // Accede a la función de traducción
 
   return (
     <div className="flex flex-col gap-5 md:gap-10">
-      <motion.p
-        initial={{ x: 100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
-        className="md:max-w-[30rem] text-md lg:text-lg"
-      >
-        {t("sections.hero.description")}
-      </motion.p>
-      <motion.div
-        initial={{ y: 100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.8, ease: "easeInOut", delay: 0.6 }}
-        className="flex flex-col gap-8"
-      >
-        <SocialLinks />
-        <div className="flex gap-2 md:gap-4">
-          <a
-            className="btn btn--red uno text-sm py-7"
-            href="/Emmanuel-Cruz-CV.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="flex items-center gap-2">
-              {t("sections.hero.cv")} CV
-              <AiOutlineDownload size={30} />
-            </span>
-          </a>
-          <EmailCopyButton />
+      <AnimatedComponent delay={2} animation="slide" direction="left">
+        <p className="md:max-w-[30rem] text-md lg:text-lg">
+          {t("sections.hero.description")}
+        </p>
+      </AnimatedComponent>
+      <AnimatedComponent delay={3} animation="slide" direction="up">
+        <div className="flex flex-col gap-8">
+          <SocialLinks />
+          <div className="flex gap-2 md:gap-4">
+            <a
+              className="btn btn--red uno text-sm py-7"
+              href="/Emmanuel-Cruz-CV.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="flex items-center gap-2">
+                {t("sections.hero.cv")} CV
+                <AiOutlineDownload size={30} />
+              </span>
+            </a>
+            <EmailCopyButton />
+          </div>
         </div>
-      </motion.div>
+      </AnimatedComponent>
     </div>
   );
 };
