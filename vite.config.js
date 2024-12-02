23ab4dff8743964bfe49d776727ba2848798a2f8
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-02 20:06:24 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -14,28 +14,6 @@ function getLastCommitDate() {
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          utils: ["i18next", "react-i18next"],
-          "yet-another-react-lightbox": ["yet-another-react-lightbox"],
-          "react-slick": ["react-slick"],
-          "slick-carousel": ["slick-carousel"],
-          emailjs: ["@emailjs/browser"],
-          "node-fetch": ["node-fetch"],
-          "react-flag-kit": ["react-flag-kit"],
-          "react-icons": ["react-icons"],
-          "react-slick": ["react-slick"],
-          "react-slick-next": ["react-slick-next"],
-          "react-slick-prev": ["react-slick-prev"],
-          "react-slick-dot": ["react-slick-dot"],
-          "react-slick-dots": ["react-slick-dots"],
-        },
-      },
-    },
-  },
   define: {
     __LAST_UPDATE__: JSON.stringify(getLastCommitDate()),
   },
