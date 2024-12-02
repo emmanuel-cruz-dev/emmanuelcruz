19d5c126653e5399a1fe9d73d328c2174333f983
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-02 10:32:54 -0300

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { lazy, Suspense } from "react";
 import "./styles/main.scss";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import ScrollToTopButton from "./components/ScrollToTopButton";
-import About from "./components/About";
-import Education from "./components/Education";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import FlipCards from "./components/FlipCards";
-import Contact from "./components/Contact";
+
+const ScrollToTopButton = lazy(() => import("./components/ScrollToTopButton"));
+const About = lazy(() => import("./components/About"));
+const Education = lazy(() => import("./components/Education"));
+const Projects = lazy(() => import("./components/Projects"));
+const Skills = lazy(() => import("./components/Skills"));
+const FlipCards = lazy(() => import("./components/FlipCards"));
+const Contact = lazy(() => import("./components/Contact"));
+
+// import ScrollToTopButton from "./components/ScrollToTopButton";
+// import About from "./components/About";
+// import Education from "./components/Education";
+// import Projects from "./components/Projects";
+// import Skills from "./components/Skills";
+// import FlipCards from "./components/FlipCards";
+// import Contact from "./components/Contact";
+
 import Footer from "./components/Footer";
 
 function App() {
@@ -15,13 +26,15 @@ function App() {
     <>
       <Header />
       <Hero />
-      <ScrollToTopButton />
-      <About />
-      <Education />
-      <Projects />
-      <Skills />
-      <FlipCards />
-      <Contact />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ScrollToTopButton />
+        <About />
+        <Education />
+        <Projects />
+        <Skills />
+        <FlipCards />
+        <Contact />
+      </Suspense>
       <Footer />
     </>
   );
