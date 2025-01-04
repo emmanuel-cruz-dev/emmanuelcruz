d81504b765380c88d6f9045b05c4c27b8ac8aab8
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2025-01-04 15:03:24 -0300

    ids agregados a objetos del array de mapeo projects en Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,6 +27,7 @@ const Projects = () => {
 
   const projects = [
     {
+      id: 1,
       title: "Los Pollos Hermanos",
       description: t("sections.projects.pollos"),
       img1: CardPollosHermanos1,
@@ -43,6 +44,7 @@ const Projects = () => {
       github: "https://github.com/EmmaLCruz/los-pollos-hermanos-website",
     },
     {
+      id: 2,
       title: "Emmanuel Cruz | Portfolio",
       description: t("sections.projects.portfolio"),
       img1: CardPortfolio1,
@@ -58,6 +60,7 @@ const Projects = () => {
       github: "https://github.com/EmmaLCruz/emmanuelcruz",
     },
     {
+      id: 3,
       title: "Jinjer | Website",
       description: t("sections.projects.jinjer"),
       img1: CardJinjer1,
@@ -73,55 +76,6 @@ const Projects = () => {
       github: "https://github.com/EmmaLCruz/jinjer",
     },
   ];
-
-  const project1 = {
-    title: "Los Pollos Hermanos",
-    description: t("sections.projects.pollos"),
-    img1: CardPollosHermanos1,
-    img2: CardPollosHermanos2,
-    img3: CardPollosHermanos3,
-    flyer: "responsive",
-    technologies: [
-      { name: "HTML", img: HTMLLogo },
-      { name: "CSS", img: CssLogo },
-      { name: "Javascript", img: JavascriptLogo },
-      { name: "Jquery", img: JqueryLogo },
-    ],
-    link: "https://emmalcruz.github.io/los-pollos-hermanos-website/",
-    github: "https://github.com/EmmaLCruz/los-pollos-hermanos-website",
-  };
-
-  const project2 = {
-    title: "Emmanuel Cruz | Portfolio",
-    description: t("sections.projects.portfolio"),
-    img1: CardPortfolio1,
-    img2: CardPortfolio2,
-    img3: CardPortfolio3,
-    flyer: "dark",
-    technologies: [
-      { name: "React", img: ReactLogo },
-      { name: "Tailwind CSS", img: TailwindLogo },
-      { name: "Sass", img: SassLogo },
-    ],
-    link: "https://emmanuel-cruz.netlify.app/",
-    github: "https://github.com/EmmaLCruz/emmanuelcruz",
-  };
-
-  const project3 = {
-    title: "Jinjer | Website",
-    description: t("sections.projects.jinjer"),
-    img1: CardJinjer1,
-    img2: CardJinjer2,
-    img3: CardJinjer3,
-    flyer: "language",
-    technologies: [
-      { name: "React", img: ReactLogo },
-      { name: "Tailwind CSS", img: TailwindLogo },
-      { name: "Sass", img: SassLogo },
-    ],
-    link: "https://jinjer.vercel.app/",
-    github: "https://github.com/EmmaLCruz/jinjer",
-  };
 
   return (
     <section
