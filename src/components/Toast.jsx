fe481fdcbea3ecd85133738a239c52d73eaace35
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-18 11:22:18 -0300

    eliminando valores mágicos de Toast

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,15 +1,24 @@
 import { FaTimes, FaCheckCircle, FaExclamationCircle } from "react-icons/fa";
 import useToast from "../hooks/useToast";
 
+// Definir constantes para los tipos de notificación
+const SUCCESS = "success";
+
+// Definir constantes para los estilos
+const ICON_COLOR_SUCCESS = "#236d37";
+const ICON_COLOR_ERROR = "#731f23";
+const TOAST_COLOR_SUCCESS = "#3ab65c";
+const TOAST_COLOR_ERROR = "#bf1d1d";
+
 const Toast = ({ message, type, show, onClose }) => {
   const { progress, isEntering, isVisibleSlide, handleClose } = useToast({
     show,
     onClose,
   });
   // Selecciona el icono y estilo según el tipo de notificación
-  const icon = type === "success" ? <FaCheckCircle /> : <FaExclamationCircle />;
-  const iconColor = type === "success" ? "#236d37" : "#731f23";
-  const toastColor = type === "success" ? "#3ab65c" : "#bf1d1d";
+  const icon = type === SUCCESS ? <FaCheckCircle /> : <FaExclamationCircle />;
+  const iconColor = type === SUCCESS ? ICON_COLOR_SUCCESS : ICON_COLOR_ERROR;
+  const toastColor = type === SUCCESS ? TOAST_COLOR_SUCCESS : TOAST_COLOR_ERROR;
 
   return (
     show && (
