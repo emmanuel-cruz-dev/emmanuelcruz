bd7d27f81e9d7d33fc5a813500f84295b3b6fb34
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-04-12 12:23:38 -0300

    refactor: organización en la disposición de los proyectos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,6 +11,10 @@ import CardPollosHermanos1 from "../assets/images/card-pollos-hermanos1.webp";
 import CardPollosHermanos2 from "../assets/images/card-pollos-hermanos2.webp";
 import CardPollosHermanos3 from "../assets/images/card-pollos-hermanos3.webp";
 
+import CardLockKey1 from "../assets/images/card-lock-key1.webp";
+import CardLockKey2 from "../assets/images/card-lock-key2.webp";
+import CardLockKey3 from "../assets/images/card-lock-key3.webp";
+
 import CardPortfolio1 from "../assets/images/card-portfolio1.webp";
 import CardPortfolio2 from "../assets/images/card-portfolio2.webp";
 import CardPortfolio3 from "../assets/images/card-portfolio3.webp";
@@ -18,10 +22,6 @@ import CardPortfolio3 from "../assets/images/card-portfolio3.webp";
 import CardJinjer1 from "../assets/images/card-jinjer1.webp";
 import CardJinjer2 from "../assets/images/card-jinjer2.webp";
 import CardJinjer3 from "../assets/images/card-jinjer3.webp";
-
-import CardLockKey1 from "../assets/images/card-lock-key1.jpg";
-import CardLockKey2 from "../assets/images/card-lock-key2.jpg";
-import CardLockKey3 from "../assets/images/card-lock-key3.jpg";
 
 import CardLiterarium1 from "../assets/images/card-literarium1.webp";
 import CardLiterarium2 from "../assets/images/card-literarium2.webp";
@@ -46,9 +46,24 @@ const getPProjects = (t: TFunction) => [
     link: "https://emmanuel-cruz-dev.github.io/los-pollos-hermanos-website/",
     github: "https://github.com/emmanuel-cruz-dev/los-pollos-hermanos-website",
   },
-
   {
     id: 2,
+    title: "LockKey | Gestor de Contraseñas",
+    description: t("sections.projects.lockKey"),
+    img1: CardLockKey1,
+    img2: CardLockKey2,
+    img3: CardLockKey3,
+    flyer: "darkMode",
+    technologies: [
+      { name: "React", img: ReactLogo },
+      { name: "TypeScript", img: TypeScriptLogo },
+      { name: "Tailwind CSS", img: TailwindLogo },
+    ],
+    link: "https://lock-key.vercel.app/",
+    github: "https://github.com/emmanuel-cruz-dev/lockkey-no-country-mvp",
+  },
+  {
+    id: 3,
     title: "Jinjer | Website",
     description: t("sections.projects.jinjer"),
     img1: CardJinjer1,
@@ -65,7 +80,7 @@ const getPProjects = (t: TFunction) => [
     github: "https://github.com/emmanuel-cruz-dev/jinjer",
   },
   {
-    id: 3,
+    id: 4,
     title: "Literarium | Bookstore",
     description: t("sections.projects.literarium"),
     img1: CardLiterarium1,
@@ -79,22 +94,6 @@ const getPProjects = (t: TFunction) => [
     ],
     link: "https://literarium.netlify.app/",
     github: "https://github.com/emmanuel-cruz-dev/literarium",
-  },
-  {
-    id: 4,
-    title: "LockKey | Gestor de Contraseñas",
-    description: t("sections.projects.lockKey"),
-    img1: CardLockKey1,
-    img2: CardLockKey2,
-    img3: CardLockKey3,
-    flyer: "language",
-    technologies: [
-      { name: "React", img: ReactLogo },
-      { name: "TypeScript", img: TypeScriptLogo },
-      { name: "Tailwind CSS", img: TailwindLogo },
-    ],
-    link: "https://lock-key.vercel.app/",
-    github: "https://github.com/emmanuel-cruz-dev/lockkey-no-country-mvp",
   },
   {
     id: 5,
