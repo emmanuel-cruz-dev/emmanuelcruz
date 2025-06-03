080b13765d323416dbb0ca02bc4f3c0e77d0e357
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-03 15:04:28 -0300

    refactor: comentarios eliminados de generateBuildInfo

--- scripts/generateBuildInfo.mjs
+++ scripts/generateBuildInfo.mjs
@@ -3,18 +3,15 @@ import { execSync } from "child_process";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
-// Obtener __dirname equivalente en ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Crear el directorio si no existe
 const buildInfoDir = join(__dirname, "../src");
 if (!fs.existsSync(buildInfoDir)) {
   fs.mkdirSync(buildInfoDir, { recursive: true });
 }
 
 try {
-  // Obtener la fecha del último commit
   const gitLastCommitDate = execSync("git log -1 --format=%cd")
     .toString()
     .trim();
@@ -23,7 +20,6 @@ try {
     lastModified: gitLastCommitDate,
   };
 
-  // Escribir el archivo buildInfo.json
   fs.writeFileSync(
     join(buildInfoDir, "buildInfo.json"),
     JSON.stringify(buildInfo, null, 2)
@@ -33,7 +29,6 @@ try {
 } catch (error) {
   console.error("Error generating build info:", error);
 
-  // Si hay un error, crear un buildInfo.json con la fecha actual
   const buildInfo = {
     lastModified: new Date().toISOString(),
   };
