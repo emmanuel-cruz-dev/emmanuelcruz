8204fc042cfaf748f20d80c7fa4a7c79eb588063
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-05 16:39:36 -0300

    feat: adds useTheme constants to constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -11,3 +11,8 @@ export const ICON_COLOR_SUCCESS = "#236d37";
 export const ICON_COLOR_ERROR = "#731f23";
 export const TOAST_COLOR_SUCCESS = "#3ab65c";
 export const TOAST_COLOR_ERROR = "#bf1d1d";
+
+// useTheme.ts
+export const THEME_DARK = "dark";
+export const THEME_LIGHT = "light";
+export const LOCAL_STORAGE_KEY = "theme";
