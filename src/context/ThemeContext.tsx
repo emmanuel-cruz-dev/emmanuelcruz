d7c39052a811ba0edf7a01007f2071b7698f934b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-05 16:33:59 -0300

    refactor: remove comments in ThemeContext.tsx

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -2,12 +2,10 @@ import { createContext, FC } from "react";
 import useThemeContext from "../hooks/useTheme";
 import { ThemeContextType, ThemeProviderProps } from "@/types/types";
 
-// Crear el contexto para el tema
 export const ThemeContext = createContext<ThemeContextType | undefined>(
   undefined
 );
 
-// Proveedor de tema
 export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
   const { theme, toggleTheme } = useThemeContext();
 
