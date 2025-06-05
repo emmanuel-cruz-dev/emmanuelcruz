3ed33afd1618082943dbdfa92a8be5f682069c4c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-05 16:29:10 -0300

    feat: adds Toast constants to constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,13 @@
+// LastModifiedDate.tsx
 export const LAST_UPDATE_TEXT_ES = "Última actualización";
 export const LAST_UPDATE_TEXT_EN = "Last update";
+
+// EmailCopyButton.tsx
 export const EMAIL = "emmanuelgerr@gmail.com";
+
+// Toast.tsx
+export const SUCCESS = "success";
+export const ICON_COLOR_SUCCESS = "#236d37";
+export const ICON_COLOR_ERROR = "#731f23";
+export const TOAST_COLOR_SUCCESS = "#3ab65c";
+export const TOAST_COLOR_ERROR = "#bf1d1d";
