be82a1082fabd3066c53889c8f6528c33135a7ab
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-05 16:31:05 -0300

    feat: exports success and errors constants to Toast.tsx

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -2,22 +2,20 @@ import { FaTimes, FaCheckCircle, FaExclamationCircle } from "react-icons/fa";
 import useToast from "../../hooks/useToast";
 import { FC } from "react";
 import { ToastProps } from "../../types/types";
-
-// Definir constantes para los tipos de notificación
-const SUCCESS = "success";
-
-// Definir constantes para los estilos
-const ICON_COLOR_SUCCESS = "#236d37";
-const ICON_COLOR_ERROR = "#731f23";
-const TOAST_COLOR_SUCCESS = "#3ab65c";
-const TOAST_COLOR_ERROR = "#bf1d1d";
+import {
+  ICON_COLOR_ERROR,
+  ICON_COLOR_SUCCESS,
+  SUCCESS,
+  TOAST_COLOR_ERROR,
+  TOAST_COLOR_SUCCESS,
+} from "../../constants/constants";
 
 const Toast: FC<ToastProps> = ({ message, type, show, onClose }) => {
   const { progress, isEntering, isVisibleSlide, handleClose } = useToast({
     show,
     onClose,
   });
-  // Selecciona el icono y estilo según el tipo de notificación
+
   const icon = type === SUCCESS ? <FaCheckCircle /> : <FaExclamationCircle />;
   const iconColor = type === SUCCESS ? ICON_COLOR_SUCCESS : ICON_COLOR_ERROR;
   const toastColor = type === SUCCESS ? TOAST_COLOR_SUCCESS : TOAST_COLOR_ERROR;
