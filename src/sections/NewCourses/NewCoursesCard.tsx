a42d730a33eb46ef2e31f6c9f48a634377fdc44a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-07 15:22:24 -0300

    style: tests background and padding styles in NewCoursesCard component

--- src/sections/NewCourses/NewCoursesCard.tsx
+++ src/sections/NewCourses/NewCoursesCard.tsx
@@ -7,20 +7,18 @@ interface NewCoursesCardProps {
 
 function NewCoursesCard({ img, alt, title, description }: NewCoursesCardProps) {
   return (
-    <article>
-      <div>
-        <figure className="w-24 h-24">
-          <img
-            className="w-full h-full cover"
-            src={img}
-            alt={alt}
-            loading="lazy"
-          />
-        </figure>
-        <div className="flip-card__content flex flex-col gap-2">
-          <h3 className="font-bold text-lg">{title}</h3>
-          <p>{description}.</p>
-        </div>
+    <article className="text-center bg-[#061d3c] px-4 py-6 h-full">
+      <figure className="w-24 h-24 mx-auto mb-2">
+        <img
+          className="w-full h-full cover"
+          src={img}
+          alt={alt}
+          loading="lazy"
+        />
+      </figure>
+      <div className="flex flex-col gap-2">
+        <h3 className="font-bold text-lg">{title}</h3>
+        <p>{description}.</p>
       </div>
     </article>
   );
