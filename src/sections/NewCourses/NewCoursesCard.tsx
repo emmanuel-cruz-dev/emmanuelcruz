d3a4b49734a12f3e82d6058d4fb9aacb71d60293
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-07 14:34:12 -0300

    feat: adds types interface to NewCoursesCard

--- src/sections/NewCourses/NewCoursesCard.tsx
+++ src/sections/NewCourses/NewCoursesCard.tsx
@@ -1,5 +1,29 @@
-function NewCoursesCard() {
-  return <div>NewCoursesCard</div>;
+interface NewCoursesCardProps {
+  img: string;
+  alt: string;
+  title: string;
+  description: string;
+}
+
+function NewCoursesCard({ img, alt, title, description }: NewCoursesCardProps) {
+  return (
+    <article>
+      <div>
+        <figure className="w-24 h-24">
+          <img
+            className="w-full h-full cover"
+            src={img}
+            alt={alt}
+            loading="lazy"
+          />
+        </figure>
+        <div className="flip-card__content flex flex-col gap-2">
+          <h3 className="font-bold text-lg">{title}</h3>
+          <p>{description}.</p>
+        </div>
+      </div>
+    </article>
+  );
 }
 
 export default NewCoursesCard;
