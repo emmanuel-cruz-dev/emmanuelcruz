279be00cace27180a6d3f076db9b320d039ea24d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-09 16:40:15 -0300

    refactor: updates styles, and imports backgroundColor constants

--- src/sections/NewCourses/NewCoursesCard.tsx
+++ src/sections/NewCourses/NewCoursesCard.tsx
@@ -1,21 +1,10 @@
-import { CourseTitle, NewCoursesCardProps } from "../../types/types";
-
-// TODO agregar colores para manejar un background personalizado en cada card
-// Colores por cada curso:
-// Academia ForIT #110b25
-// Alura Latam #061d3c
-// Talento Tech #153242
+import { backgroundColor } from "../../constants/constants";
+import { NewCoursesCardProps } from "../../types/types";
 
 function NewCoursesCard({ img, alt, title, description }: NewCoursesCardProps) {
-  const backgroundColor: Record<CourseTitle, string> = {
-    "Academia ForIT": "bg-[#110b25]",
-    "Alura Latam": "bg-[#061d3c]",
-    "Talento Tech": "bg-[#153242]",
-  };
-
   return (
     <article
-      className={`text-center ${backgroundColor[title]} px-9 py-7 h-full text-white`}
+      className={`text-center ${backgroundColor[title]} px-9 pt-7 pb-9 h-full text-white`}
     >
       <figure className="w-24 h-24 mx-auto mb-2">
         <img
@@ -26,8 +15,10 @@ function NewCoursesCard({ img, alt, title, description }: NewCoursesCardProps) {
         />
       </figure>
       <div className="flex flex-col gap-2">
-        <h3 className="font-bold text-lg">{title}</h3>
-        <p className="leading-7 w-10/12 mx-auto md:w-auto">{description}.</p>
+        <h3 className="font-bold text-lg lg:text-2xl">{title}</h3>
+        <p className="leading-7 w-10/12 mx-auto md:w-auto text-gray-300">
+          {description}.
+        </p>
       </div>
     </article>
   );
