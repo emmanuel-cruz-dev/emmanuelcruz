6e6d0a6d926885cee426fd7f751820e770acd1e5
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-09 16:52:56 -0300

    refactor: imports newCourses from data folder

--- src/sections/NewCourses/NewCourses.tsx
+++ src/sections/NewCourses/NewCourses.tsx
@@ -1,33 +1,6 @@
+import { newCourses } from "../../data/newCourses";
 import AnimatedComponent from "../../components/ui/AnimatedComponent";
 import NewCoursesCard from "./NewCoursesCard";
-import ForITLogo from "../../assets/icons/fotIT-logo.webp";
-import TalentoTechLogo from "../../assets/icons/talento-tech-logo.webp";
-import AluraLatamLogo from "../../assets/icons/alura-latam-logo.webp";
-import { NewCoursesCardProps } from "@/types/types";
-
-const newCourses: NewCoursesCardProps[] = [
-  {
-    img: AluraLatamLogo,
-    alt: "Icono Alura Latam",
-    title: "Alura Latam",
-    description:
-      "Formación en desarrollo backend con Java y Spring Boot, incluyendo bases de datos relacionales con MySQL y despliegue en Oracle Cloud",
-  },
-  {
-    img: ForITLogo,
-    alt: "Icono Academia ForIT",
-    title: "Academia ForIT",
-    description:
-      "Enfoque en buenas prácticas de desarrollo con TypeScript, uso avanzado de Git, Docker, metodologías TDD, Clean Code y arquitectura limpia",
-  },
-  {
-    img: TalentoTechLogo,
-    alt: "Icono Talento Tech",
-    title: "Talento Tech",
-    description:
-      "Especialización en desarrollo backend con Node.js, Express y Firestore, trabajando con el ecosistema moderno de JavaScript y Firebase",
-  },
-];
 
 function NewCourses() {
   return (
@@ -45,7 +18,6 @@ function NewCourses() {
         </div>
       </AnimatedComponent>
       <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-8">
-        {/* Cards */}
         {newCourses.map((course, index) => (
           <NewCoursesCard key={index} {...course} />
         ))}
