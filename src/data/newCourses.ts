5a09868027d2a6afbec20e3ae48337f7692f4b53
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-11 15:24:40 -0300

    fix: updates getNewCourses types

--- src/data/newCourses.ts
+++ src/data/newCourses.ts
@@ -1,10 +1,10 @@
-// import { NewCoursesCardProps } from "../types/types";
 import ForITLogo from "../assets/icons/fotIT-logo.webp";
 import TalentoTechLogo from "../assets/icons/talento-tech-logo.webp";
 import AluraLatamLogo from "../assets/icons/alura-latam-logo.webp";
 import { TFunction } from "i18next";
+import { NewCoursesCardProps } from "../types/types";
 
-const getNewCourses = (t: TFunction) => [
+const getNewCourses = (t: TFunction): NewCoursesCardProps[] => [
   {
     img: AluraLatamLogo,
     alt: "Icono Alura Latam",
