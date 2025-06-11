12aa944af0b31ffbbaa372abe2bae9237a04bb1a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-11 15:27:57 -0300

    feat: adds translation to title and subtitle in NewCourses component

--- src/sections/NewCourses/NewCourses.tsx
+++ src/sections/NewCourses/NewCourses.tsx
@@ -1,8 +1,12 @@
-import { newCourses } from "../../data/newCourses";
+import getNewCourses from "../../data/newCourses";
 import AnimatedComponent from "../../components/ui/AnimatedComponent";
 import NewCoursesCard from "./NewCoursesCard";
+import { useTranslation } from "react-i18next";
 
 function NewCourses() {
+  const { t } = useTranslation();
+  const newCourses = getNewCourses(t);
+
   return (
     <section
       className="scroll-top py-10 flex flex-col gap-8 w-11/12 xl:w-10/12 mx-auto select-none"
@@ -10,11 +14,10 @@ function NewCourses() {
     >
       <AnimatedComponent animation="fade" delay={1}>
         <div className="text-center mb-5">
-          <h2 className="font-bold text-3xl mb-4">Actualmente Cursando</h2>
-          <h3 className="text-lg">
-            Ampliando mis conocimientos de desarrollo Backend y Fullstack a
-            través de estos cursos.
-          </h3>
+          <h2 className="font-bold text-3xl mb-4">
+            {t("sections.newCourses.title")}
+          </h2>
+          <h3 className="text-lg">{t("sections.newCourses.description")}.</h3>
         </div>
       </AnimatedComponent>
       <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-8">
