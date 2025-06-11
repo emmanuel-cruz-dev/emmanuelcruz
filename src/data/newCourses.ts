13d88ddef3de3dcfd5ff86a3a80af7eaed1af2cc
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-11 15:10:52 -0300

    feat: modifies newCourses array into a function getNewCourses in src/data/newCourses.ts

--- src/data/newCourses.ts
+++ src/data/newCourses.ts
@@ -1,28 +1,28 @@
-import { NewCoursesCardProps } from "../types/types";
+// import { NewCoursesCardProps } from "../types/types";
 import ForITLogo from "../assets/icons/fotIT-logo.webp";
 import TalentoTechLogo from "../assets/icons/talento-tech-logo.webp";
 import AluraLatamLogo from "../assets/icons/alura-latam-logo.webp";
+import { TFunction } from "i18next";
 
-export const newCourses: NewCoursesCardProps[] = [
+const getNewCourses = (t: TFunction) => [
   {
     img: AluraLatamLogo,
     alt: "Icono Alura Latam",
     title: "Alura Latam",
-    description:
-      "Formación en desarrollo backend con Java y Spring Boot, incluyendo bases de datos relacionales con MySQL y despliegue en Oracle Cloud",
+    description: t("sections.newCourses.aluraLatam"),
   },
   {
     img: ForITLogo,
     alt: "Icono Academia ForIT",
     title: "Academia ForIT",
-    description:
-      "Enfoque en buenas prácticas de desarrollo con TypeScript, uso avanzado de Git, Docker, metodologías TDD, Clean Code y arquitectura limpia",
+    description: t("sections.newCourses.academiaForIt"),
   },
   {
     img: TalentoTechLogo,
     alt: "Icono Talento Tech",
     title: "Talento Tech",
-    description:
-      "Especialización en desarrollo backend con Node.js, Express y Firestore, trabajando con el ecosistema moderno de JavaScript y Firebase",
+    description: t("sections.newCourses.talentoTech"),
   },
 ];
+
+export default getNewCourses;
