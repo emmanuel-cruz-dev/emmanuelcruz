c71d42edb16193e9f88c2fd51e78cdc9049c3e07
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-12 08:54:27 -0300

    style: modifies scale property in article SkillsCard

--- src/sections/Skills/SkillsCard.tsx
+++ src/sections/Skills/SkillsCard.tsx
@@ -1,14 +1,14 @@
-import { SkillsCardProps } from "@/types/types";
+import { SkillsCardProps } from "../../types/types";
 import { FC } from "react";
 
 const SkillsCard: FC<SkillsCardProps> = ({ src, alt, title }) => {
   return (
-    <article className="card" title={title}>
+    <article className="card group" title={title}>
       <img
         src={src}
         alt={`${alt} logo`}
         loading="lazy"
-        className="w-16 hover:scale-110 transition-all duration-300 ease-in-out"
+        className="w-16 group-hover:scale-110 transition-all duration-300 ease-in-out"
         width="100"
         height="100"
       />
