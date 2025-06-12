ec1ee906abd8099dd17c1de04d711ed25d86264a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-12 08:23:30 -0300

    feat: adds and maps webSkills in Web Development box in Skills.tsx

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -1,10 +1,6 @@
 import AnimatedComponent from "../../components/ui/AnimatedComponent";
 import { useTranslation } from "react-i18next";
 
-import HTMLLogo from "../../assets/icons/html-logo.png";
-import CssLogo from "../../assets/icons/css-logo.png";
-import JavascriptLogo from "../../assets/icons/javascript-logo.png";
-import TypescriptLogo from "../../assets/icons/typescript-logo.png";
 import ReactLogo from "../../assets/icons/react-logo.png";
 import ReactNativeLogo from "../../assets/icons/react-native-logo.png";
 import AngularLogo from "../../assets/icons/angular-logo.png";
@@ -30,10 +26,12 @@ import BemLogo from "../../assets/icons/bem-logo.png";
 import SkillsCard from "./SkillsCard";
 import SkillsContainer from "./SkillsContainer";
 import { useTheme } from "../../hooks/useThemeProvider";
+import { getWebSkills } from "../../data/skills";
 
 function Skills() {
   const { t } = useTranslation();
   const { theme } = useTheme();
+  const webSkills = getWebSkills(t);
 
   return (
     <main className="scroll-top py-10 w-11/12 mx-auto select-none" id="skills">
@@ -49,29 +47,9 @@ function Skills() {
         <article className="flex flex-col gap-8 justify-center items-center w-full lg:flex-row lg:flex-wrap">
           {/* Desarrollo Web */}
           <SkillsContainer title={t("sections.skills.subtitle1")}>
-            <SkillsCard
-              title={t("sections.skills.html")}
-              src={HTMLLogo}
-              alt="HTML"
-            />
-
-            <SkillsCard
-              title={t("sections.skills.css")}
-              src={CssLogo}
-              alt="CSS"
-            />
-
-            <SkillsCard
-              title={t("sections.skills.javascript")}
-              src={JavascriptLogo}
-              alt="Javascript"
-            />
-
-            <SkillsCard
-              title={t("sections.skills.typescript")}
-              src={TypescriptLogo}
-              alt="TypeScript"
-            />
+            {webSkills.map((skill, index) => (
+              <SkillsCard key={index} {...skill} />
+            ))}
           </SkillsContainer>
 
           {/* Frameworks y Bibliotecas */}
@@ -124,46 +102,6 @@ function Skills() {
               alt="Bootstrap"
             />
           </SkillsContainer>
-          {/* <article
-              className={`${
-                theme === "dark" ? "shadow__dark" : "shadow__light"
-              } w-full md:w-fit flex flex-col gap-8 items-center text-center bg-sky-300/20 py-6 px-10 border-2 border-sky-300/20 hover:bg-transparent transition-colors duration-300`}
-            >
-              <h4 className="font-semibold text-xl">
-                {t("sections.skills.subtitle2")}
-              </h4>
-              <article className="flex flex-wrap gap-6 lg:gap-14 justify-around items-center">
-                <SkillsCard
-                  title={t("sections.skills.react")}
-                  src={ReactLogo}
-                  alt="React"
-                />
-
-                <SkillsCard
-                  title={t("sections.skills.nodejs")}
-                  src={NodeLogo}
-                  alt="Node.js"
-                />
-
-                <SkillsCard
-                  title={t("sections.skills.tailwind")}
-                  src={TailwindLogo}
-                  alt="Tailwind CSS"
-                />
-
-                <SkillsCard
-                  title={t("sections.skills.sass")}
-                  src={SassLogo}
-                  alt="Sass"
-                />
-
-                <SkillsCard
-                  title={t("sections.skills.bootstrap")}
-                  src={BootstrapLogo}
-                  alt="Bootstrap"
-                />
-              </article>
-            </article> */}
 
           {/* Herramientas de Desarrollo */}
           <SkillsContainer title={t("sections.skills.subtitle3")}>
