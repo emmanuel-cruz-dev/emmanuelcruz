bf911ca944fa064445f69e639515907de76f78ad
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-12 10:00:19 -0300

    refactor: updates imports in SkillsContainer

--- src/sections/Skills/SkillsContainer.tsx
+++ src/sections/Skills/SkillsContainer.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { useTheme } from "../../hooks/useThemeProvider";
 import { SkillsContainerProps } from "../../types/types";
 
