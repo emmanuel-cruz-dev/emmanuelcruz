e72549bae4d81e0af1573b3da39e683a981154c7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-13 16:33:20 -0300

    fix: updates estructure in formatText component

--- src/helpers/formatText.tsx
+++ src/helpers/formatText.tsx
@@ -6,9 +6,9 @@ export const formatTextWithLabels = (text: string): React.JSX.Element => {
   const content = text.substring(colonIndex + 1);
 
   return (
-    <span>
+    <p>
       <span className="font-semibold text-neutral-300/90">{label}</span>
-      <span className="font-normal">{content}</span>
-    </span>
+      <span className="font-normal">{content}.</span>
+    </p>
   );
 };
