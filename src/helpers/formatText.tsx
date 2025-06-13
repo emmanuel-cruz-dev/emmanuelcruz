84a0867ccb1e346783c4c6212c0759e2e508c0c9
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-13 16:08:13 -0300

    feat: adds helpers folder and formatText.tsx file inside that folder

---
+++ src/helpers/formatText.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const formatTextWithLabels = (text: string): React.JSX.Element => {
+  const colonIndex = text.indexOf(":");
+  const label = text.substring(0, colonIndex + 1);
+  const content = text.substring(colonIndex + 1);
+
+  return (
+    <span>
+      <span className="font-semibold text-neutral-300/90">{label}</span>
+      <span className="font-normal">{content}</span>
+    </span>
+  );
+};
