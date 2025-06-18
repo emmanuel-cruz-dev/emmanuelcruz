e5ba8bebf8440117a2fa0a5fb104e0efedda43ea
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-18 07:58:44 -0300

    feat: adds aria-label in SocialLinks anchors

--- src/components/ui/SocialLinks.tsx
+++ src/components/ui/SocialLinks.tsx
@@ -9,25 +9,27 @@ function SocialLinks() {
         href="https://www.linkedin.com/in/emmanuel-cruz-dev/"
         target="_blank"
         rel="noopener noreferrer"
+        aria-label="Linkedin"
       >
         <FaLinkedin className="w-full h-full" />
       </a>
       <a
-        title="Github"
+        title="GitHub"
         className="social__links"
         href="https://github.com/emmanuel-cruz-dev"
         target="_blank"
         rel="noopener noreferrer"
+        aria-label="GitHub"
       >
         <FaGithub className="w-full h-full" />
       </a>
-
       <a
         title="Email"
         className="social__links"
         href="mailto:emmanuelgerr@gmail.com"
         target="_blank"
         rel="noopener noreferrer"
+        aria-label="Email"
       >
         <FaPaperPlane className="w-full h-full" />
       </a>
