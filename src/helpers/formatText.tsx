f73575a895e64dd9540e75ebf4e30a4550e44370
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-09 16:50:16 -0300

    fix: updates text colors in FormatTextWithLabels component

--- src/helpers/formatText.tsx
+++ src/helpers/formatText.tsx
@@ -1,13 +1,21 @@
+import { useTheme } from "../hooks/useThemeProvider";
 import React from "react";
 
-export const formatTextWithLabels = (text: string): React.JSX.Element => {
+export const FormatTextWithLabels = (text: string): React.JSX.Element => {
   const colonIndex = text.indexOf(":");
   const label = text.substring(0, colonIndex + 1);
   const content = text.substring(colonIndex + 1);
+  const { theme } = useTheme();
 
   return (
     <p>
-      <span className="font-semibold text-neutral-300/90">{label}</span>
+      <span
+        className={`font-semibold ${
+          theme === "dark" ? "text-gray-300" : "text-gray-600"
+        }`}
+      >
+        {label}
+      </span>
       <span className="font-normal">{content}.</span>
     </p>
   );
