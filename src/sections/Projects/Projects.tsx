725203def3b65c79537de045565955a9640e27fa
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-08-13 16:57:25 -0300

    refactor:replace divs with semantic HTML tags (header, main) in Projects component

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -13,20 +13,20 @@ function Projects() {
       id="projects"
     >
       <AnimatedComponent animation="fade" delay={1}>
-        <div className="text-center mb-5">
+        <header className="text-center mb-5">
           <h2 className="font-bold text-3xl mb-4">
             {t("sections.projects.title")}
           </h2>
           <h3 className="text-lg">{t("sections.projects.description")}</h3>
-        </div>
+        </header>
       </AnimatedComponent>
       <article>
-        <div className="card-container flex flex-wrap gap-12 mx-auto">
+        <main className="card-container flex flex-wrap gap-12 mx-auto">
           {/* Mapeo de proyectos */}
           {projects.map((project, index) => (
             <ProjectsCard key={index} {...project} />
           ))}
-        </div>
+        </main>
       </article>
     </section>
   );
