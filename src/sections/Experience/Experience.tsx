9518ab692011257faba387ec394510f737e0d5ca
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-19 16:17:48 -0300

    refactor: updates code order in src/sections/Experience/Experience.tsx

--- src/sections/Experience/Experience.tsx
+++ src/sections/Experience/Experience.tsx
@@ -1,7 +1,7 @@
+import { useTranslation } from "react-i18next";
 import { workExperiences } from "../../data/workExperiences";
 import AnimatedComponent from "../../components/ui/AnimatedComponent";
 import Timeline from "./TimeLine";
-import { useTranslation } from "react-i18next";
 
 function Experience() {
   const { t } = useTranslation();
