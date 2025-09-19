9aa70e0f65b515a990e921c0d3991a93dc0153e1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-19 15:38:40 -0300

    feat: updates TimeLine component structure in src/sections/Experience/TimeLine.tsx

--- src/sections/Experience/TimeLine.tsx
+++ src/sections/Experience/TimeLine.tsx
@@ -1,33 +1,28 @@
 import { FC } from "react";
-import { workExperiences } from "../../data/workExperiences";
 import { getSortedExperiences } from "../../utils/utils";
 import { TimelineProps } from "../../types/types";
 import TimelineItem from "./TimeLineItem";
 
-const Timeline: FC<TimelineProps> = ({ experiences = workExperiences }) => {
+const Timeline: FC<TimelineProps> = ({ experiences }) => {
   const sortedExperiences = getSortedExperiences(experiences);
 
   return (
-    <article className="bg-gray-900 text-white">
-      <div className="mx-auto px-4">
-        {/* Timeline Container */}
-        <div className="relative max-w-6xl mx-auto">
-          {sortedExperiences.map((experience, index) => (
-            <TimelineItem
-              key={experience.id}
-              experience={experience}
-              isLast={index === experiences.length - 1}
-            />
-          ))}
-        </div>
+    <article className="mx-auto px-4">
+      <main className="relative max-w-6xl mx-auto">
+        {sortedExperiences.map((experience, index) => (
+          <TimelineItem
+            key={experience.id}
+            experience={experience}
+            isLast={index === experiences.length - 1}
+          />
+        ))}
+      </main>
 
-        <a
-          href="#contact"
-          className="inline-block mt-16 px-8 py-3 bg-emerald-600 hover:bg-emerald-500 text-white font-semibold rounded-lg transition-colors duration-200 transform hover:scale-105"
-        >
+      <footer className="flex justify-center mt-16">
+        <a href="#contact" className="btn py-7 hover:bg-[#ff0033]">
           Contáctame
         </a>
-      </div>
+      </footer>
     </article>
   );
 };
