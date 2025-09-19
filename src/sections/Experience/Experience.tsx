cf8fcb5b28d81dbddfa3565997fe24bc4e1d23dd
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-19 15:08:57 -0300

    feat: adds animated component to src/sections/Experience/Experience.tsx

--- src/sections/Experience/Experience.tsx
+++ src/sections/Experience/Experience.tsx
@@ -1,3 +1,4 @@
+import AnimatedComponent from "../../components/ui/AnimatedComponent";
 import Timeline from "./TimeLine";
 
 function Experience() {
@@ -6,16 +7,16 @@ function Experience() {
       className="scroll-top py-10 flex flex-col gap-8 w-11/12 mx-auto select-none"
       id="experience"
     >
-      <div className="text-center mb-5">
-        <h2 className="font-bold text-3xl mb-4">Experiencia Laboral</h2>
-        <h3 className="text-lg">
-          Participación activa en equipos de trabajo aplicando metodologías
-          ágiles y contribuyendo con código limpio.
-        </h3>
-      </div>
-      <article className="w-full">
-        <Timeline />
-      </article>
+      <AnimatedComponent animation="fade" delay={1}>
+        <header className="text-center mb-5">
+          <h2 className="font-bold text-3xl mb-4">Experiencia Laboral</h2>
+          <h3 className="text-lg">
+            Participación activa en equipos de trabajo aplicando metodologías
+            ágiles y contribuyendo con código limpio.
+          </h3>
+        </header>
+      </AnimatedComponent>
+      <Timeline />
     </section>
   );
 }
