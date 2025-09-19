128079cf83f666b88bbc5225f038e91ef6107a5c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-19 14:00:16 -0300

    feat: adds workExperiences to src/data/

---
+++ src/data/workExperiences.ts
@@ -0,0 +1,73 @@
+import { Experience } from "../types/types";
+
+export const workExperiences: Experience[] = [
+  {
+    id: 1,
+    position: "Frontend Developer",
+    company: "No Country",
+    period: "Febrero - Marzo | 2025",
+    description:
+      "Desarrollo de Lock Key utilizando Vite, React, TypeScript y Tailwind CSS. Implementación de interfaces responsivas y optimización de rendimiento. Colaboración en equipos ágiles.",
+    github: "https://github.com/NoCountry/lock-key",
+    technologies: ["Vite", "React", "TypeScript", "Tailwind CSS"],
+    side: "left",
+  },
+  {
+    id: 2,
+    position: "Desarrollador Frontend",
+    company: "No Country",
+    period: "Marzo - Abril | 2025",
+    description:
+      "Desarrollo de FitFlow web corporativos y e-commerce. Maquetación de diseños usando HTML5, CSS3 y JavaScript vanilla. Integración con CMS como WordPress y optimización SEO básica.",
+    github: "https://github.com/NoCountry/fitflow",
+    technologies: ["Vite", "React", "TypeScript", "Tailwind CSS"],
+    side: "right",
+  },
+  {
+    id: 3,
+    position: "Desarrollador Frontend",
+    company: "Foo Talent Group",
+    period: "Abril - Junio | 2025",
+    description:
+      "Desarrollo de Aura, plataforma integral de gestión de registros y atenciones para el centro terapéutico Rayuela. Creación de landing pages efectivas y sitios web responsive.",
+    github: "https://github.com/FooTalentGroup/aura",
+    technologies: ["Vite", "React", "TypeScript", "Tailwind CSS"],
+    side: "left",
+  },
+  {
+    id: 4,
+    position: "Desarrollador Frontend",
+    company: "Kramer E-Commerce",
+    period: "Junio - 2025 | Presente",
+    description:
+      "Desarrollo de aplicación E-Commerce multiplataforma junto a equipo multidisciplinario.",
+    github: "https://github.com/KramerE-Commerce/Kramer-E-Commerce",
+    technologies: [
+      "Vite",
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Axios",
+      "React-Query",
+    ],
+    side: "right",
+  },
+  {
+    id: 5,
+    position: "Desarrollador Frontend",
+    company: "Foo Talent Group",
+    period: "Julio - Septiembre | 2025",
+    description:
+      "Desarrollo de Flowik, software de gestión de inventario para productos de mascotas.",
+    github: "https://github.com/FooTalentGroup/flowik",
+    technologies: [
+      "Vite",
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Axios",
+      "React-Query",
+    ],
+    side: "left",
+  },
+];
